0d7e647ff75a2f24025f3e7398c7c5fd941dde24
bonox2 <fontoften@gmail.com>
2021-05-01 09:57:43 -0400

    form done

--- app.js
+++ app.js
@@ -50,112 +50,29 @@ async function getData(url, cb) {
     }
 }
 
-
-
-function renderTableData(elem, data) {
-    elem.innerHTML = ''
-    let covidDataHtml = '' //переменная равна строке
-    data.forEach(country => {
-      covidDataHtml += createTableRow(country)
+function addMessages(elem, messages) {
+let messagesHtml = '' //переменная равна строке
+    messages.forEach(message => {//функцыя для каждого елемента
+    messagesHtml += renderMessage(message)
     });
-    elem.innerHTML = covidDataHtml//приравниваем
+    elem.innerHTML = messagesHtml//приравниваем
   }
-  
-  function createSummary(dataArray) {
-    const summaryCounts = {
-      confirmed: 0,
-      deaths: 0,
-      recovered: 0,
-      existing: 0,
-      delta_confirmed: 0,
-      delta_deaths: 0,
-      delta_recovered: 0,
-      delta_existing: 0,
-    }
-    const summaryCountsKeys = Object.keys(summaryCounts)
-  
-    dataArray.forEach(dataObj => {
-      summaryCountsKeys.forEach(key => {
-        summaryCounts[key] += dataObj[key]
-      })
-    })
-    return `
-    <div class="confirmed">
-      <h3 class="text-color">Confirmed:</h3>
-      <div>
-        ${numberFormatter.format(summaryCounts.confirmed)}
-      </div>
-      <div>
-        ${summaryCounts.delta_confirmed !== 0 ? `<span>${summaryCounts.delta_confirmed < 0 ? "&#9660;" : "&#9650;"}</span>` : ''}
-        ${summaryCounts.delta_confirmed === 0 ? "-" : numberFormatter.format(summaryCounts.delta_confirmed)}
-      </div>
-    </div>
-    <div class="deaths">
-      <h3 class="text-color">Deaths:</h3>
-      <div>
-        ${numberFormatter.format(summaryCounts.deaths)}
-      </div>
-      <div>
-        ${summaryCounts.delta_deaths !== 0 ? `<span>${summaryCounts.delta_deaths < 0 ? "&#9660;" : "&#9650;"}</span>` : ''}
-        ${summaryCounts.delta_deaths === 0 ? "-" : numberFormatter.format(summaryCounts.delta_deaths)}
-      </div>
-    </div>
-    <div class="recovered">
-      <h3 class="text-color">Recovered:</h3>
-      <div>
-        ${numberFormatter.format(summaryCounts.recovered)}
-      </div>
-      <div>
-        ${summaryCounts.delta_recovered !== 0 ? `<span>${summaryCounts.delta_recovered < 0 ? "&#9660;" : "&#9650;"}</span>` : ''}
-        ${summaryCounts.delta_recovered === 0 ? "-" : numberFormatter.format(summaryCounts.delta_recovered)}
-      </div>
-    </div>
-    <div class="existing">
-      <h3 class="text-color">Existing:</h3>
-      <div>
-        ${numberFormatter.format(summaryCounts.existing)}
-      </div>
-      <div>
-        ${summaryCounts.delta_existing !== 0 ? `<span>${summaryCounts.delta_existing < 0 ? "&#9660;" : "&#9650;"}</span>` : ''}
-        ${summaryCounts.delta_existing === 0 ? "-" : numberFormatter.format(summaryCounts.delta_existing)}
-      </div>
-    </div>
-    `
-  }
-  function createTableRow(data) {
+function renderMessage(data) {//каркас секции
     const html =
-      `<tr>
-          <td>${data.label[lang]}</td>
-          <td class="confirmed">
-            <div>${numberFormatter.format(data.confirmed)}</div>
-            <div>
-                ${data.delta_confirmed !== 0 ? `<span>${data.delta_confirmed < 0 ? "&#9660;" : "&#9650;"}</span>` : ''}
-              ${data.delta_confirmed === 0 ? "-" : numberFormatter.format(data.delta_confirmed)}
+        `<div class="letter-section ${data.seen ? 'seen' : 'not_seen'}" data-id="${data.id}">
+            <div class="sender-info">
+                <div class="senders-photo"><img width="50" height="50" loading="lazy" src="${data.avatar}" alt="${data.name}"></div>
+                <div>
+                    <div class="sender-name">${data.name}</div>
+                    <div class="sender-number">${data.phone}</div>
+                </div>
             </div>
-          </td>
-          <td class="deaths">
-            <div>${numberFormatter.format(data.deaths)}</div>
-            <div>
-                ${data.delta_deaths !== 0 ? `<span>${data.delta_deaths < 0 ? "&#9660;" : "&#9650;"}</span>` : ''}
-              ${data.delta_deaths === 0 ? "-" : numberFormatter.format(data.delta_deaths)}
+            <div class="message-info"> ${data.text}</div>
+            <div class="date-time">
+                <div class="time">${timeFormat.format(data.date)}</div>
+                <div class="time">${dateFormat.format(data.date)}</div>
             </div>
-          </td>
-          <td class="recovered">
-            <div>${numberFormatter.format(data.recovered)}</div>
-            <div>
-                ${data.delta_recovered !== 0 ? `<span>${data.delta_recovered < 0 ? "&#9660;" : "&#9650;"}</span>` : ''}
-              ${data.delta_recovered === 0 ? "-" : numberFormatter.format(data.delta_recovered)}
-            </div>
-          </td>
-          <td class="existing">
-            <div>${numberFormatter.format(data.existing)}</div>
-            <div>
-                ${data.delta_existing !== 0 ? `<span>${data.delta_existing < 0 ? "&#9660;" : "&#9650;"}</span>` : ''}
-              ${data.delta_existing === 0 ? "-" : numberFormatter.format(data.delta_existing)}
-            </div>
-          </td>
-        </tr>
-            `
-    return html
-  }
-  
+        </div>
+    </div>`
+    return html//возвращаем результат
+}
